3aaaa1a24f134f1468aa5f02056b9ea697d36806
abdullarahimli <abdullavr@code.edu.az>
2022-02-10 00:54:31 +0400

    First commit

---
+++ CustomSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FindChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ədədlərdən ibarət array qəbul edən method yazın.
+            // Array sort metoduna uyğun CustomSort(int[] arr) metodunu yazın.
+
+            int a = int.Parse(Console.ReadLine());
+
+            int b = int.Parse(Console.ReadLine());
+
+            int c = int.Parse(Console.ReadLine());
+
+            int[] arr = { a, b, c };
+
+            CustomSort(arr);
+        }
+        public static void CustomSort(int[] a)
+        {
+            Console.Write("DAxil etdiyiniz forma");
+
+            foreach (int value in a)
+            {
+                Console.Write(value + " ");
+            }
+
+            Console.WriteLine("      ");
+
+            Console.Write("Donusturulmus forma");
+
+            Array.Sort(a);
+
+            foreach (int value in a)
+            {
+                Console.Write(value + " ");
+            }
+        }
+    }
+}
---
+++ FindChar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FindChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Stringlerden ibaret array-i və char qəbul edən metod yazın.
+			// Arrayin hansi indexin-də bu qəbul edilən char varsa, o string-ləri ekranda yazdirin.
+
+			char x = char.Parse(Console.ReadLine());
+            string y = Console.ReadLine();
+            string[] text = y.Split(" ");
+
+
+			FindChar(text, x);
+        }
+
+
+		public static void FindChar(string[] a, char b)
+		{
+
+			for (int i = 0; i < a.Length; i++)
+			{
+				string c = a[i];
+
+				for (int j = 0; j < c.Length; j++)
+				{
+					if (c[j] == b)
+					{
+						Console.WriteLine(c);
+						break;
+					}
+
+				}
+
+			}
+
+		}
+	}
+}
